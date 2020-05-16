5998ac2b6849df36f2ce90bdfc74b1068c043c86
Noura <n.almusained@hotmail.com>
2020-05-16 08:58:04 +0300

    Create te.py

---
+++ te.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter number 1:"))
+num2 = int(input("enter number 2:"))
+if num1>=num2:
+ print("sorry cant result you shold select another numb1 more than num2")
+
+else:
+ print("the result is", num1 + num2)
